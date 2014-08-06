51d38c86c1e942e7fcf7572af2b382a5bad5624a
MarcusM0 <https://github.com/MarcusM0/lottery.git>
2014-08-06 15:08:57 +0800

    de

--- js/login.js
+++ js/login.js
@@ -1,36 +1,32 @@
 $().ready(function() {
 	$("#formLogin").validate({
 		submitHandler: function(e) {
-		   document.getElementById("formLogin").submit();
-		   
-//		   $("#msg").html("");
-//           var url=$("#formLogin").attr("url");
-//           var email=$("#email").val();
-//           var password=$("#password").val();
-//           $.post(url,{email:email,password:password},function(result){
-//        	   var result=eval("("+result+")");
-//        	   alert(result.code);
-//        	   if(result.code==true){
-//        		   document.location.href=result.url;
-//        	   }else{
-//        		   $("#msg").html("用户名或密码有误");
-//        	   }
-//           });     
+		   $("#msg").html("");
+           var url=$("#formLogin").attr("url");
+           var email=$("#email").val();
+           var password=$("#password").val();
 
-
+           $.post(url,{email:email,password:password},function(result){
+        	   var result=eval("("+result+")");
+        	   if(result.code==true){
+        		   document.location.href=result.url;
+        	   }else{
+        		   $("#msg").html("用户名或密码有误");
+        	   }
+           });     
 		},		
 		rules: {
 		    email: {
 		       required: true,
 		       email: true
-	        },
+	        },       
 			password: "required"
 		},
 		messages: {
 			email: {
 			    required: "邮箱不能为空!",
 			    email: "邮箱格式不正确!"
-    	    },
+    	    },	    
 			password: "密码不能为空!"
 		}
 	});		
